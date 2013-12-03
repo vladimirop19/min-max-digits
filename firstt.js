54ed8575d514020d72b8745d00ec8eded6df2508
Vladimir Opatski <vovasik94@gmail.com>
2013-12-03 19:07:07 +0400

    Небольшое изменение оформления кода

--- firstt.js
+++ firstt.js
@@ -1,19 +1,17 @@
 /**
  * Created by vladimir on 12/3/13.
  */
-var a = 1837;
+var a = 1878;
 var b=0;
 var c =0;
 var d =10;
-if (typeof (a) == "number")
-{
+if (typeof (a) == "number"){
 while (a>0){
     c=a % 10;
     if (c>b){
         b=c;
     }
-    if (c<d)
-    {
+    if (c<d){
         d=c;
     }
     a=(a-a%10)/10;
@@ -21,7 +19,6 @@ while (a>0){
 console.log('Наименьшая цифра числа=',d);
 console.log('Наибольшая цифра числа=',b);
 }
-else
-{
+else{
     console.log('На обработку поступило не число')
 }
