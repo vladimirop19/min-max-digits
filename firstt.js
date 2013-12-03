e2453e72fd74e6ed411a34d22634dc85d7791f02
Vladimir Opatski <vovasik94@gmail.com>
2013-12-03 18:52:35 +0400

    Первая попытка

---
+++ firstt.js
@@ -0,0 +1,27 @@
+/**
+ * Created by vladimir on 12/3/13.
+ */
+var a = 1837;
+var b=0;
+var c =0;
+var d =10;
+if (typeof (a) == "number")
+{
+while (a>0){
+    c=a % 10;
+    if (c>b){
+        b=c;
+    }
+    if (c<d)
+    {
+        d=c;
+    }
+    a=(a-a%10)/10;
+}
+console.log('Наименьшая цифра числа=',d);
+console.log('Наибольшая цифра числа=',b);
+}
+else
+{
+    console.log('На обработку поступило не число')
+}
