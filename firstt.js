532d8ad752149e687b9ca8d4bc4b273ee79ebe6c
Vladimir Opatski <vovasik94@gmail.com>
2013-12-05 17:17:42 +0400

    Fixed issues #1 #2 #3

--- firstt.js
+++ firstt.js
@@ -5,21 +5,21 @@ var num = 1878;
 var max = 0;
 var min = 10;
 if (typeof (num) == 'number') {
-if (num < 0) {
+  if (num < 0) {
     num = 0 - num;
-}
-while (num > 0) {
+  }
+  while (num > 0) {
     if (num % 10 > max) {
-        max = num % 10;
+      max = num % 10;
     }
     if (num % 10 < min) {
-        min = num % 10;
+      min = num % 10;
     }
     num = (num - num % 10) / 10;
-}
-console.log('Наименьшая цифра числа=', min);
-console.log('Наибольшая цифра числа=', max);
+  }
+  console.log('Наименьшая цифра числа=', min);
+  console.log('Наибольшая цифра числа=', max);
 }
 else {
-    console.log('На обработку поступило не число');
+  console.log('На обработку поступило не число');
 }
